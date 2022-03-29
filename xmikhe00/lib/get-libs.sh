cb62aee34227787500fe51741ca44c8e8274d87e
VladMikheda <vladmikheda@gmail.com>
2022-03-29 02:56:12 +0200

    new lib files

--- xmikhe00/lib/get-libs.sh
+++ xmikhe00/lib/get-libs.sh
@@ -15,10 +15,12 @@ wget https://repo1.maven.org/maven2/org/openjfx/javafx-fxml/18/javafx-fxml-18.ja
 wget https://repo1.maven.org/maven2/org/openjfx/javafx-fxml/18/javafx-fxml-18-linux.jar
 wget https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/18/javafx-graphics-18.jar
 wget https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/18/javafx-graphics-18-linux.jar
-wget https://download2.gluonhq.com/openjfx/18/openjfx-18_linux-x64_bin-sdk.zip
-unzip openjfx-18_linux-x64_bin-sdk.zip
-rm -rf openjfx-18_linux-x64_bin-sdk.zip
-
+# wget https://download2.gluonhq.com/openjfx/18/openjfx-18_linux-x64_bin-sdk.zip
+wget https://download2.gluonhq.com/openjfx/18/openjfx-18_monocle-linux-x64_bin-sdk.zip
+# unzip openjfx-18_linux-x64_bin-sdk.zip
+# rm -rf openjfx-18_linux-x64_bin-sdk.zip
+unzip openjfx-18_monocle-linux-x64_bin-sdk.zip
+rm -rf openjfx-18_monocle-linux-x64_bin-sdk.zip
 # mvn install:install-file\
 # -DlocalRepositoryPath=lib\
 # -Dfile=lib/javafx-controls-18.jar\
