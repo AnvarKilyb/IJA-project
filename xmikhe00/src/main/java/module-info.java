ec18bc92c770c637470879f33b193b48981962fe
VladMikheda <vladmikheda@gmail.com>
2022-03-30 03:33:10 +0200

    add new files

--- xmikhe00/src/main/java/module-info.java
+++ xmikhe00/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ija.diagram.gui {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
+    requires json.simple;
     opens ija.diagram.gui;
     exports ija.diagram.gui;
 }
