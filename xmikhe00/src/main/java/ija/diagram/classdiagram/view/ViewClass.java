d6e7bce6b183f4d9b0d3a3c0a6e776023fced01c
VladMikheda <vladmikheda@gmail.com>
2022-04-10 19:40:39 +0200

    refactoring

--- xmikhe00/src/main/java/ija/diagram/classdiagram/view/ViewClass.java
+++ xmikhe00/src/main/java/ija/diagram/classdiagram/view/ViewClass.java
@@ -11,47 +11,86 @@ public class ViewClass extends VBox {
 
     private VBox arguments;
     private VBox methods;
+    private TextField className = new TextField("Name");
+    private Label mainLabel = new Label("              ");
     ContextMenu contextMenu = new ContextMenu();
 
     public ViewClass(){
         super();
     }
 
-    public void show(){
-        Label label = new Label("Class");
-        this.addContextMenu();
-        label.setContextMenu(contextMenu);
-        super.getChildren().add(label);
-        super.getChildren().add(new TextField("Name"));
+    private void setVisualOptions(){
         super.setWidth(100);
         super.setHeight(100);
         super.setStyle("-fx-background-color: black;");
+    }
+
+    private void mainConstructClass(){
+        this.addContextMenu();
+        mainLabel.setContextMenu(contextMenu);
+        super.getChildren().add(mainLabel);
+        super.getChildren().add(className);
         arguments = new VBox();
         methods = new VBox();
         super.getChildren().add(arguments);
         super.getChildren().add(methods);
-//        ClassItem item = new ClassItem();
-//        item.show();
-//        arguments.getChildren().add(item);
-        //todo add interception click right mouses button
-        super.addEventFilter(MouseEvent.MOUSE_DRAGGED, ViewClassController::classDraggedMouse);
-//        super.addEventFilter(MouseEvent.MOUSE_CLICKED, ViewClassController::classMenu);
     }
 
     private void addContextMenu(){
         MenuItem menuItem1 = new MenuItem("Add new argument");
         menuItem1.setUserData(this);
-        menuItem1.addEventHandler(Event.ANY, ClassContextController::add_item);
+        menuItem1.addEventHandler(Event.ANY, ClassContextController::addArgument);
         MenuItem menuItem2 = new MenuItem("Add new method");
-
+        menuItem2.setUserData(this);
+        menuItem2.addEventHandler(Event.ANY, ClassContextController::addMethod);
         contextMenu.getItems().addAll(menuItem1,menuItem2);
-
     }
 
+    public void show(){
+        this.mainConstructClass();
+        this.setVisualOptions();
+        super.addEventFilter(MouseEvent.MOUSE_DRAGGED, ViewClassController::classDraggedMouse);
+    }
+//------------------------------------------------------------------------------------------------------//
     public void addArgument(){
-        ClassItem classItem = new ClassItem();
-        classItem.show(this);
-        arguments.getChildren().add(classItem);
+        ArgumentItem argumentItem = new ArgumentItem();
+        argumentItem.mainConstruct();
+        arguments.getChildren().add(argumentItem);
+        System.out.println(className.getPrefWidth());
+    }
+    public void addMethod(){
+        MethodItem methodItem = new MethodItem();
+        methodItem.mainConstruct();
+        methods.getChildren().add(methodItem);
     }
 
+    public void addArgument(String argumentName, String accessModifier, String typeName){
+        ArgumentItem argumentItem = new ArgumentItem();
+        argumentItem.mainConstruct();
+        argumentItem.setArgumentName(argumentName);
+        argumentItem.setArgumentType(typeName);
+        argumentItem.setAccessModifier(accessModifier);
+        arguments.getChildren().add(argumentItem);
+
+    }
+
+    public void addMethod(String methodName, String accessModifier){
+        MethodItem methodItem = new MethodItem();
+        methodItem.mainConstruct();
+        methodItem.setMethodName(methodName);
+        methodItem.setAccessModifier(accessModifier);
+        methods.getChildren().add(methodItem);
+    }
+
+    public void setX(int x){
+        super.setLayoutX(x);
+    }
+
+    public void setY(int y){
+        super.setLayoutY(y);
+    }
+
+    public void setClassName(String className){
+        this.className.setText(className);
+    }
 }
--- xmikhe00/src/main/java/ija/diagram/classdiagram/view/ViewDiagram.java
+++ xmikhe00/src/main/java/ija/diagram/classdiagram/view/ViewDiagram.java
@@ -1,16 +1,45 @@
 package ija.diagram.classdiagram.view;
 
+import ija.diagram.classdiagram.model.Arguments;
 import ija.diagram.classdiagram.model.DiagramClass;
-import ija.diagram.classdiagram.view.ViewClass;
-
-import java.util.Collection;
+import ija.diagram.classdiagram.model.Item;
+import ija.diagram.classdiagram.model.Methods;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ViewDiagram {
-//    private HashMap viewAndClass =  new HashMap<ViewClass, DiagramClass>();
-//
-//    public void addClass(){
-//        ViewClass viewClass = new ViewClass();
-//    }
+
+    private final Map<ViewClass, DiagramClass> diagramClassMap = new HashMap<ViewClass, DiagramClass>();
+
+
+    public void addNewClass(DiagramClass modelClass){
+        ViewClass viewClass = new ViewClass();
+        String className = modelClass.getName();
+        List<Arguments> argumentsList = modelClass.getArgumentsList();
+        List<Methods> methodsList = modelClass.getMethodsList();
+        viewClass.setClassName(className);
+        addArguments(viewClass, argumentsList);
+        addMethods(viewClass, methodsList);
+        diagramClassMap.put(viewClass, modelClass);
+        //todo x, y
+    }
+
+    private void addArguments(ViewClass viewClass, List<Arguments> argumentsList){
+        for(Arguments argument : argumentsList){
+            String name = argument.getName();
+            String type = argument.getType();
+            String accessModifierString = Item.returnString(argument.getAccessModifier());
+            viewClass.addArgument(name, accessModifierString, type);
+        }
+    }
+
+    private void addMethods(ViewClass viewClass, List<Methods> methodsList){
+        for(Methods method : methodsList){
+            String name = method.getName();
+            String accessModifierString = Item.returnString(method.getAccessModifier());
+            viewClass.addMethod(name, accessModifierString);
+        }
+    }
+
 }
