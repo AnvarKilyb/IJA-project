49582a098a78eaa22d23d6e7f17b05b9548621bc
VladMikheda <vladmikheda@gmail.com>
2022-04-10 19:38:17 +0200

    add delete

--- xmikhe00/src/main/java/ija/diagram/classdiagram/controller/ClassContextController.java
+++ xmikhe00/src/main/java/ija/diagram/classdiagram/controller/ClassContextController.java
@@ -1,25 +1,31 @@
 package ija.diagram.classdiagram.controller;
 
-import ija.diagram.classdiagram.view.ClassItem;
 import ija.diagram.classdiagram.view.ViewClass;
+import ija.diagram.classdiagram.view.ViewItem;
 import javafx.event.Event;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 
 public class ClassContextController {
 
-    public static void add_item(Event mouseEvent){
+    public static void addArgument(Event mouseEvent){
         MenuItem menuItem = (MenuItem) mouseEvent.getSource();
         ViewClass viewClass = (ViewClass) menuItem.getUserData();
         viewClass.addArgument();
     }
 
-    public static void delete_item(Event mouseEvent){
+    public static void addMethod(Event mouseEvent){
         MenuItem menuItem = (MenuItem) mouseEvent.getSource();
-        ClassItem classItem = (ClassItem) menuItem.getUserData();
-        VBox vBox = (VBox) classItem.getParent();
+        ViewClass viewClass = (ViewClass) menuItem.getUserData();
+        viewClass.addMethod();
+    }
+
+    public static void deleteItem(Event mouseEvent){
+        MenuItem menuItem = (MenuItem) mouseEvent.getSource();
+        ViewItem viewItem = (ViewItem) menuItem.getUserData();
+        VBox vBox = (VBox) viewItem.getParentItem();
 //        System.out.println("sdsd");
-        vBox.getChildren().remove(classItem);
+        vBox.getChildren().remove(viewItem);
     }
 
     public static void show_contextmenu_arguments(Event mouseEvent){
